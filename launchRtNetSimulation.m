8611231ade78cd9493ca83789f62a7f9936fdc12
Mattia Lecci <mattia.lecci@gmail.com>
2021-11-09 16:06:26 -0500

    Add documentation

--- launchRtNetSimulation.m
+++ launchRtNetSimulation.m
@@ -1,4 +1,58 @@
 function out = launchRtNetSimulation(scenarioPath, params)
+% Simulate the link-layer based on a given qd-realization scenario.
+% The details of the simulation are passed by the params struct.
+%
+% channel taps for the given scenario based on the parameters.
+% Taps are reduced to complex scalars by applying the TX/RX beamforming
+% vectors to the channel matrix, for each node pair and for each time
+% stamp.
+% IN:
+%   - scenarioPath: the path to the qd-realization scenario to process
+%   - params: the parameters for the link-layer simulation. They comprise:
+%     * processRatios (bool): ratios are SNR, SINR, SIR, INR
+%     * saveHref (bool): saves all channel matrices of the reference
+%     transmission. warning: the output can be extremely large!
+%     * saveScalarTaps (bool): scalar taps contain delay and complex (IQ) gain
+%     * bsAnt (classes.Antenna): Antenna configuration for base stations
+%     * utAnt (classes.Antenna): Antenna configuration for users
+%     * bsIdxs: Indexes of base station nodes, corresponding to NodePosition(idx).dat
+%     * utIdxs: Indexes of user nodes, corresponding to NodePosition(idx).dat
+%     * txRefIdx: Node idx of the reference transmitter
+%     * rxRefIdx: Node idx of the reference receiver
+%     * dataDirection: "DL" or "UL"
+%     * bsInterfIdxs: Node idxs of the interfering base station(s)
+%     * utInterfIdxs: Node idxs of the interfering user(s)
+%     * bsSectorDir: Antenna azimuth direction for base stations [rad] (untested)
+%     * bsDowntilt: Antenna downtilt for base stations: pi/2 points at the horizon [rad]
+%     * utSectorDir: Antenna azimuth direction for users [rad] (untested)
+%     * utDowntilt: Antenna downtilt for users: pi/2 points at the horizon [rad]
+%     * Ptx: Transmitted power [dBm]
+%     * fc: Carrier frequency [Hz]
+%     * BW: Bandiwdth [Hz]
+%     * F: Noise figure [dB]
+%     * bfMode: "SVD", "geometric", or "codebook"
+%     * bsCodebookFile: path to codebook used by all base stations (if
+%     bfMode == "codebook")
+%     * utCodebookFile: path to codebook used by all users (if
+%     bfMode == "codebook")
+% OUT:
+%   out (struct): contains relevant link-layer metrics to the reference link:
+%     * Prx_dbm (tTot, 1): received power
+%     * I_dbm (tTot, 1): interference power
+%     * sv1 (tTot, 1): largest singular value of Href
+%     * sv2 (tTot, 1): second largest singular value of Href
+%     * channelGenTime (tTot, 1): time to generate Href
+%     * N_dbm: noise power
+%     * fullSimTime: simulation time to run the whole function
+%     * INR_db (tTot, 1): Interference-to-Noise ratio (if
+%     params.processRatios)
+%     * SNR_db (tTot, 1): Signal-to-Noise ratio (if params.processRatios)
+%     * SIR_db (tTot, 1): Signal-to-Interference ratio (if params.processRatios)
+%     * SINR_db (tTot, 1): Signal-to-(Interference and Noise) ratio (if
+%     params.processRatios)
+%     * Href (nAntRx, nAntTx, tTot): channel matrices (if params.saveHref)
+%     * scalarTaps (tTot, 1): list of struct containing fields 'delay' [s] and
+%     'iq' [linear] of the same length (if params.saveScalarTaps)
 
 
 assert(isValidScenarioPath(scenarioPath),...
